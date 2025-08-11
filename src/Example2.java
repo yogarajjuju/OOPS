4782ee5cca5e0a177183b77465fb9db0c422ee18
Yogaraj <yogarajjuju22@gmail.com>
2025-08-11 17:43:49 +0530

    bankai

--- src/Example2.java
+++ src/Example2.java
@@ -2,11 +2,14 @@ public class Example2 {
     public static void main(String[] args) {
         Students st= new Students();
         Students juju = new Students();
+        Students random1 = new Students();
 //        System.out.println(juju);
         System.out.println(juju.roll);
         System.out.println(juju.name);
         System.out.println(juju.marks);
         juju.greet();
+
+        System.out.println(random1.name);
 
     }
 }
@@ -23,6 +26,9 @@ class Students {
         this.name ="juju";
         this.marks=89;
     } //this keyword is used to act as a linker to shorter the code size
+//    Students(){
+//        this(13,"default",100.0f);
+//    }
 
 }
 

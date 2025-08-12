ff5fbe524a43bbb4df579bb0c9e26f1df704144a
Yogaraj <yogarajjuju22@gmail.com>
2025-08-12 14:24:24 +0530

    bankai

---
+++ src/pack/a/b/Greet.java
@@ -0,0 +1,10 @@
+package pack.a.b;
+
+import static pack.a.b.Message.message;
+
+public class Greet {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        message();
+    }
+}
---
+++ src/pack/a/b/Message.java
@@ -0,0 +1,11 @@
+package pack.a.b;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+    public static  void message(){
+        System.out.println("hi");
+
+    }
+}

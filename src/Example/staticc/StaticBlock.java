c771dc31ae1281750db6ff7600374f4722939f04
Yogaraj <yogarajjuju22@gmail.com>
2025-08-12 18:04:18 +0530

    bankai

--- src/Example/staticc/StaticBlock.java
+++ src/Example/staticc/StaticBlock.java
@@ -1,9 +1,10 @@
 package Example.staticc;
+// This is a example to show to init the static variables
 
 public class StaticBlock {
     static  int a = 10;
     static int b;
-
+// will only run once , when the first obj is created i.e  when the class is loaded for the first time .
     static {
         System.out.println("this is the static block");
         b = a*2;
@@ -12,6 +13,11 @@ public class StaticBlock {
     public static void main(String[] args) {
         StaticBlock obj = new StaticBlock();
         System.out.println(StaticBlock.a+" "+ StaticBlock.b);
+        StaticBlock.b+=3;
+        System.out.println(StaticBlock.a+" "+ StaticBlock.b);
+        StaticBlock obj2 = new StaticBlock();
+        System.out.println(StaticBlock.a+" "+ StaticBlock.b);
+
     }
 
 }

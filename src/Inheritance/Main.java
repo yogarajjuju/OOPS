126475582a152d298a787fffd0b594c18380c8ee
Juju <yogarajjuju22@gmail.com>
2025-08-21 14:37:31 +0530

    banaki

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -8,7 +8,26 @@ public class Main {
 //        System.out.println(box2.h+" "+ box2.l+" "+ box2.w);
        Weight box3 = new Weight();
        Weight box4 = new Weight(2,3,4,8);
-       
+        //        System.out.println(box1.w + " " + box1.h);
+
+//        BoxWeight box3 = new BoxWeight();
+//        BoxWeight box4 = new BoxWeight(2, 3, 4, 8);
+//        System.out.println(box3.h + " " + box3.weight);
+
+
+//        Box box5 = new BoxWeight(2, 3, 4, 8);
+//        System.out.println(box5.w);
+
+        // there are many variables in both parent and child classes
+        // you are given access to variables that are in the ref type i.e. BoxWeight
+        // hence, you should have access to weight variable
+        // this also means, that the ones you are trying to access should be initialised
+        // but here, when the obj itself is of type parent class, how will you call the constructor of child class
+        // this is why error
+//        BoxWeight box6 = new Box(2, 3, 4);
+//        System.out.println(box6);
+
+
 
     }
 }

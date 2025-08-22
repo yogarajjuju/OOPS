ef318e71d1b9a05da4087d8d9b1fbd1ebf691ed1
Juju <yogarajjuju22@gmail.com>
2025-08-22 14:11:32 +0530

    banaki

--- src/Polymorphism/Circle.java
+++ src/Polymorphism/Circle.java
@@ -1,6 +1,10 @@
 package Polymorphism;
 
 public class Circle extends Shapes {
+    //this will run  when obj  of circle is created
+    // hence it is overriding the parent method
+    @Override// this is called annotation
+    // this is used to check whether a method is override or not :
     void area(){
         System.out.println("The area is pie * r*r");
     }

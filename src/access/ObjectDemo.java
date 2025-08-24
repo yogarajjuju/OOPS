a07f4052ef07eb1f3f5dff3036e82ed4bf8e4c8c
Yogaraj <yogarajjuju22@gmail.com>
2025-08-24 19:06:54 +0530

    bankai

--- src/access/ObjectDemo.java
+++ src/access/ObjectDemo.java
@@ -29,6 +29,7 @@ public class ObjectDemo {
         System.out.println(obj.hashCode());// the hashcode used to identify the unique value of obj
         System.out.println(obj2.hashCode());// the hashcode used to identify the unique value of obj(this has different value of hash code)
         //i.e it is just a random value  to find the uniqueness of the object:
+        System.out.println(obj.getClass());//use the get the class it belongs
     }
 
 //    @Override
@@ -40,5 +41,6 @@ public class ObjectDemo {
 //    protected void finalize() throws Throwable {
 //        super.finalize();
 //    }
+
 }
 

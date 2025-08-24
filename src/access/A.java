f0479830a4c472c38b72f9335c4923a684565232
yogarajjuj <yogarajjuju22@gmail.com>
2025-08-24 15:02:19 +0530

    banakai

---
+++ src/access/A.java
@@ -0,0 +1,21 @@
+package access;
+
+public class A {
+   private int num;
+    String name;
+    int [] arr;
+
+    public int getNum(int num) {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public A(int num, String name) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[34];
+    }
+}
---
+++ src/access/Main.java
@@ -0,0 +1,14 @@
+package access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(34, "IJK");
+
+        // need to do a few things;
+        // 1 . access the data members;
+        // 2.  modify the data members;
+
+//        obj.getNum();
+
+    }
+}

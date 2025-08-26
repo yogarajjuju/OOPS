0a6170d04d3eec7f599dfc9a6df8219194679e6f
yogarajjuj <yogarajjuju22@gmail.com>
2025-08-26 11:11:54 +0530

    banakai

--- src/generics/LambdaFuctions.java
+++ src/generics/LambdaFuctions.java
@@ -7,9 +7,10 @@ public class LambdaFuctions {
         ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             arr.add(i+1);
-            arr.forEach((item)-> System.out.println(item*2));
+
 
         }
+        arr.forEach((item)-> System.out.println(item*2));
     }
 
 

c998ee85264f288ae895615b6be0af159cc6f77d
yogarajjuj <yogarajjuju22@gmail.com>
2025-08-28 10:32:34 +0530

    banakai

---
+++ src/Collections/Main.java
@@ -0,0 +1,30 @@
+package Collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list= new ArrayList<>();
+        List<Integer> list2= new LinkedList<>();
+        list2.add(20);
+        list2.add(50);
+        list2.add(66);
+        list2.add(47);
+        System.out.println(list2);
+
+        List<Integer>vector = new Vector<>();
+        vector.add(10);
+        vector.add(15);
+        vector.add(32);
+        vector.add(14);
+        System.out.println("This is vector");
+        System.out.println(vector);
+
+
+
+
+
+
+
+    }
+}
